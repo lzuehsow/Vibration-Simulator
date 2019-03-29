149cb46046f9afe446fa58531b76500ab49cf687
Lydia Z <lydiazuehsow@gmail.com>
2019-03-29 14:10:09 -0400

    Simplified ODE

--- suspension.m
+++ suspension.m
@@ -16,7 +16,7 @@ L_inv = inv(L);
 
 K_tilda = L_inv * K * L_inv;
 C_tilda = L_inv * C * L_inv;
-[eigvec,eigval] = eig(K_tilda)
+[eigvec,eigval] = eig(K_tilda);
 
 P = eigvec;
 
@@ -29,10 +29,10 @@ diag = transpose(P) * K_tilda * P;
 t_span = linspace(0,10);
 x_init = [0.2 0; 0.1 0];
 
-x0 = x_init(1) % m
-theta0 = x_init(2) % rad
-vx0 = x_init(3) % m/s
-vtheta0 = x_init(4) % rad/s
+x0 = x_init(1); % m
+theta0 = x_init(2); % rad
+vx0 = x_init(3); % m/s
+vtheta0 = x_init(4); % rad/s
 
 Z_0 = [x0, theta0, vx0, vtheta0];     %specify initial conditions (rads, rads/sec)
 
@@ -43,9 +43,13 @@ res = zeros(4,1);
 res(1) = Z(3); % dx2dt
 res(2) = Z(4); % dTheta2dt
 
-% % Original
-res(3) = (-(k1 + k2) * Z(1) + (k1*l1 - k2*l2) * Z(2)) / m;
-res(4) = ((l1*k1 - l2*k2) * Z(1) - ((l1^2)*k1 + (l2^2)*k2) * Z(2)) / I;
+% % Undamped
+% res(3) = (-(k1 + k2) * Z(1) + (k1*l1 - k2*l2) * Z(2)) / m;
+% res(4) = ((l1*k1 - l2*k2) * Z(1) - ((l1^2)*k1 + (l2^2)*k2) * Z(2)) / I;
+
+K = [k1+k2 (-k1*l1 + k2*l2); (-k1*l1 + k2*l2) ((l1^2)*k1 + (l2^2)*k2)];
+res(3) = (-K(1) * Z(1) + -K(2) * Z(2)) / m;
+res(4) = (-K(3) * Z(1) - K(4) * Z(2)) / I;
 end
 
 %% Analytical Soln
@@ -56,16 +60,18 @@ q_init = transpose(P) * L * x_init;
 zeta = ((C / K) * eigval) / (2 * sqrt(eigval));
 
 % Find damped natural frequency
-Wd = sqrt(eigval) * sqrt ([1 1; 1 1] - (zeta^2));
+Wd = sqrt(eigval) * sqrt ([1 0; 0 1] - (zeta.^2));
 
 % Equation of motion in q space
 undamped = [q_init(1) * cos(sqrt(eigval(1)) * t_span) + (q_init(3)/sqrt(eigval(1))) * sin(sqrt(eigval(2)) * t_span); ...
 q_init(2) * cos(sqrt(eigval(4)) * t_span) + (q_init(4)/sqrt(eigval(4))) * sin(sqrt(eigval(2)) * t_span)];
 
-underdamped = exp(-sqrt(eigval) * t_span) .* (q_init(1) * cos(Wd(1) * t_span) + () + sin(Wd(2) * t))
-+ ((q_init(:,2) + zeta * sqrt(eigval) * q_init)/Wd) * sin(Wd * t_span))];
+underdamped = [exp(-zeta(1) * sqrt(eigval(1)) * t_span) .* ...
+    (q_init(1) * cos(Wd(1) * t_span) + ((q_init(2) + zeta(1) * sqrt(eigval(1)) * q_init(1)) / Wd(1)) * sin(Wd(1) * t_span)); ...
+    exp(-zeta(4) * sqrt(eigval(4)) * t_span) .* ...
+    (q_init(3) * cos(Wd(4) * t_span) + ((q_init(4) + zeta(4) * sqrt(eigval(3)) * q_init(3)) / Wd(4)) * sin(Wd(4) * t_span))];
 
-% % Equation of motion converted to x space
+% Equation of motion converted to x space
 undamped_analytical = L_inv * P * undamped;
 damped_analytical = L_inv * P * underdamped;
 
